c56b1b0af76ffec7283ec5db8248784f0b34be01
aabdelsameia1 <167092699+aabdelsameia1@users.noreply.github.com>
2024-06-24 15:59:58 +0200

    0.3.0

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='myinternshipcalculator2024',
-    version='0.2.9',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[],
     author='Abdallah Abdelsameia',
     author_email='aabdelsameia1@gmail.com',
     description='A simple internship hours calculator.',
-    # long_description=open('README.md').read(),
-    # long_description_content_type='text/markdown',
     url='https://github.com/aabdelsameia1/myinternshipcalculator2024',
     classifiers=[
         'Programming Language :: Python :: 3',
