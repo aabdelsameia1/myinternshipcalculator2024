19ac527ca5d41a8170a1b8ea6526a3b6eecdaf5a
aabdelsameia1 <167092699+aabdelsameia1@users.noreply.github.com>
2024-07-02 17:18:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='myinternshipcalculator2024',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(),
     install_requires=[],
     author='Abdallah Abdelsameia',
